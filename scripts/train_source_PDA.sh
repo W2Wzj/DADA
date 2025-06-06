e46cf258116cac7e480e060e2b47c060784fb30b
W2Wzj <124041961+W2Wzj@users.noreply.github.com>
2025-06-06 17:20:58 +0800

    Add files via upload

---
+++ scripts/train_source_PDA.sh
@@ -0,0 +1,23 @@
+gpuid=${1:-0}
+random_seed=${2:-2021}
+
+# export CUDA_VISIBLE_DEVICES=$gpuid
+
+echo "OSDA SOURCE TRAIN ON OFFICE"
+# python train_source.py  --dataset Office --s_idx 0  --target_label_type PDA --epochs 50 --lr 0.01 
+# python train_source.py  --dataset Office --s_idx 1  --target_label_type PDA --epochs 50 --lr 0.01 
+# python train_source.py  --dataset Office --s_idx 2  --target_label_type PDA --epochs 50 --lr 0.01 
+
+
+echo "OSDA SOURCE TRAIN ON OFFICEHOME"
+# python train_source.py  --dataset OfficeHome --s_idx 0  --target_label_type PDA --epochs 50 --lr 0.01 
+# python train_source.py  --dataset OfficeHome --s_idx 1  --target_label_type PDA --epochs 50 --lr 0.01 
+# python train_source.py  --dataset OfficeHome --s_idx 2  --target_label_type PDA --epochs 50 --lr 0.01 
+# python train_source.py  --dataset OfficeHome --s_idx 3  --target_label_type PDA --epochs 50 --lr 0.01 
+
+
+echo "OSDA SOURCE TRAIN ON VisDA"
+python train_source.py --backbone_arch resnet101 --dataset VisDA --s_idx 0  --target_label_type PDA --epochs 15 --lr 0.001 --gpu 0
+
+
+
---
+++ scripts/train_target_IBA_PDA.sh
@@ -0,0 +1,26 @@
+
+echo "PDA ON Office"
+python train_target_IBA.py --dataset Office --s_idx 0 --t_idx 1 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 1.10   
+python train_target_IBA.py --dataset Office --s_idx 0 --t_idx 2 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 1.10
+python train_target_IBA.py --dataset Office --s_idx 1 --t_idx 0 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset Office --s_idx 1 --t_idx 2 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 1.10
+python train_target_IBA.py --dataset Office --s_idx 2 --t_idx 0 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset Office --s_idx 2 --t_idx 1 --lr 0.001 --target_label_type PDA --lam 1.0 --alpha 1 --beta 1.10
+
+echo "PDA ON Office-Home"
+python train_target_IBA.py --dataset OfficeHome --s_idx 0 --t_idx 1 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 0 --t_idx 2 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 0 --t_idx 3 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 1 --t_idx 0 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 1 --t_idx 2 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 1 --t_idx 3 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 2 --t_idx 0 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 2 --t_idx 1 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 2 --t_idx 3 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 3 --t_idx 0 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 3 --t_idx 1 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+python train_target_IBA.py --dataset OfficeHome --s_idx 3 --t_idx 2 --lr 0.001 --target_label_type PDA --lam 2.0 --alpha 1 --beta 0.80
+
+echo "PDA ON VisDA"
+python train_target_IBA.py --backbone_arch resnet101 --lr 0.0001 --dataset VisDA --target_label_type PDA --s_idx 0 --t_idx 1 --lam 1.0 --alpha 1 --beta 0.80 --epochs 30
+
